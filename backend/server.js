4b9e6060cb039e18c28dc21526d272b4f978d906
TalaatEysa <talaateysa@outlook.com>
2025-05-07 22:12:10 +0300

    implement custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
